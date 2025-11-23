edb21c7aedc93341dcff915333fbbb3cbf866541
refparo <ref.paro@outlook.com>
2025-11-23 17:32:59 +0800

    Better organize the code and optimize

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::fs::{read, read_to_string};
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, LazyLock, Mutex};
 
 use chrono::Datelike;
 use typst::diag::{FileError, FileResult};
@@ -10,38 +10,6 @@ use typst::text::{Font, FontBook};
 use typst::utils::LazyHash;
 use typst::{Feature, Library, LibraryExt, World};
 use typst_kit::fonts::{FontSearcher, FontSlot, Fonts};
-
-pub struct BlogWorld {
-  library: LazyHash<Library>,
-  book: LazyHash<FontBook>,
-  sources: Mutex<HashMap<FileId, Source>>,
-  files: Mutex<HashMap<FileId, Bytes>>,
-  fonts: Vec<FontSlot>,
-}
-
-impl BlogWorld {
-  pub fn new() -> BlogWorld {
-    let library = Library::builder()
-      .with_features([Feature::Html].into_iter().collect())
-      .build();
-    let Fonts { book, fonts } = FontSearcher::new().search();
-    BlogWorld {
-      library: LazyHash::new(library),
-      book: LazyHash::new(book),
-      sources: Mutex::new(HashMap::new()),
-      files: Mutex::new(HashMap::new()),
-      fonts,
-    }
-  }
-
-  pub fn with_main(self: Arc<Self>, main: FileId) -> BlogWorldWithMain {
-    BlogWorldWithMain {
-      world: self,
-      main,
-      now: chrono::Utc::now(),
-    }
-  }
-}
 
 pub struct BlogWorldWithMain {
   world: Arc<BlogWorld>,
@@ -55,7 +23,7 @@ impl World for BlogWorldWithMain {
   }
 
   fn book(&self) -> &LazyHash<FontBook> {
-    &self.world.book
+    &self.world.fonts.book
   }
 
   fn main(&self) -> FileId {
@@ -63,50 +31,58 @@ impl World for BlogWorldWithMain {
   }
 
   fn source(&self, id: FileId) -> FileResult<Source> {
-    let sources = self.world.sources.lock().unwrap();
-    if let Some(source) = sources.get(&id) {
-      Ok(source.clone())
+    let mut files = self.world.files.lock().unwrap();
+    if let Some(file) = files.get_mut(&id) {
+      file.source()
     } else {
-      drop(sources);
+      // I think it's better to take the risk of duplicating reads
+      // rather than making everyone else wait for this read to finish.
+      // Benchmarking also shows that even in serialized cases,
+      // releasing the lock and re-acquiring after the read finishes is faster
+      // than keeping the lock for the duration. (Why?)
+      drop(files);
       let path = id.vpath().as_rooted_path();
       let buf =
         read_to_string(path).map_err(|err| FileError::from_io(err, path))?;
-      Ok(
-        self
-          .world
-          .sources
-          .lock()
-          .unwrap()
-          .entry(id)
-          .or_insert_with(|| Source::new(id, buf))
-          .clone(),
-      )
+      let source = Source::new(id, buf);
+      use std::collections::hash_map::Entry;
+      match self.world.files.lock().unwrap().entry(id) {
+        Entry::Occupied(entry) => entry.into_mut().source(),
+        Entry::Vacant(entry) => {
+          entry.insert(FileSlot::new(FileSlotCache::Source(source.clone())));
+          Ok(source)
+        }
+      }
     }
   }
 
   fn file(&self, id: FileId) -> FileResult<Bytes> {
-    let files = self.world.files.lock().unwrap();
-    if let Some(file) = files.get(&id) {
-      Ok(file.clone())
+    let mut files = self.world.files.lock().unwrap();
+    if let Some(file) = files.get_mut(&id) {
+      Ok(file.bytes())
     } else {
       drop(files);
       let path = id.vpath().as_rooted_path();
       let buf = read(path).map_err(|err| FileError::from_io(err, path))?;
-      Ok(
-        self
-          .world
-          .files
-          .lock()
-          .unwrap()
-          .entry(id)
-          .or_insert_with(|| Bytes::new(buf))
-          .clone(),
-      )
+      let bytes = Bytes::new(buf);
+      use std::collections::hash_map::Entry;
+      match self.world.files.lock().unwrap().entry(id) {
+        Entry::Occupied(entry) => Ok(entry.into_mut().bytes()),
+        Entry::Vacant(entry) => {
+          entry.insert(FileSlot::new(FileSlotCache::Bytes(id, bytes.clone())));
+          Ok(bytes)
+        }
+      }
     }
   }
 
   fn font(&self, index: usize) -> Option<Font> {
-    self.world.fonts.get(index).and_then(|slot| slot.get())
+    self
+      .world
+      .fonts
+      .fonts
+      .get(index)
+      .and_then(|slot| slot.get())
   }
 
   fn today(&self, offset: Option<i64>) -> Option<Datetime> {
@@ -125,5 +101,104 @@ impl World for BlogWorldWithMain {
       date.month0() as u8 + 1,
       date.day0() as u8 + 1,
     )
+  }
+}
+
+pub struct BlogWorld {
+  library: LazyHash<Library>,
+  fonts: LazyLock<LazyHashFonts>,
+  files: Mutex<HashMap<FileId, FileSlot>>,
+}
+
+impl BlogWorld {
+  pub fn new() -> BlogWorld {
+    let library = Library::builder()
+      .with_features([Feature::Html].into_iter().collect())
+      .build();
+    BlogWorld {
+      library: LazyHash::new(library),
+      // Since we only emit HTML, it may seem that we don't need fonts support.
+      // However, remember that our pages might contain `#html.frame`s.
+      // We lazily search the fonts for faster startup.
+      fonts: LazyLock::new(|| LazyHashFonts::new(FontSearcher::new().search())),
+      files: Mutex::new(HashMap::new()),
+    }
+  }
+
+  pub fn with_main(self: Arc<Self>, main: FileId) -> BlogWorldWithMain {
+    BlogWorldWithMain {
+      world: self,
+      main,
+      now: chrono::Utc::now(),
+    }
+  }
+}
+
+// We need this wrapper until the changes in `Fonts` are published in a release.
+struct LazyHashFonts {
+  book: LazyHash<FontBook>,
+  fonts: Vec<FontSlot>,
+}
+
+impl LazyHashFonts {
+  fn new(fonts: Fonts) -> LazyHashFonts {
+    LazyHashFonts {
+      book: LazyHash::new(fonts.book),
+      fonts: fonts.fonts,
+    }
+  }
+}
+
+struct FileSlot {
+  // stale: bool,
+  cache: FileSlotCache,
+}
+
+enum FileSlotCache {
+  Bytes(FileId, Bytes),
+  Source(Source),
+  Both(Source, Bytes),
+}
+
+impl FileSlot {
+  fn new(cache: FileSlotCache) -> FileSlot {
+    FileSlot {
+      // stale: false,
+      cache,
+    }
+  }
+
+  // There is a redundant `clone()` in both of the following functions.
+  // However, we can't eliminate them without using `unsafe` or introducing
+  // an invalid state to `FileSlotCache`.
+  fn bytes(&mut self) -> Bytes {
+    match &self.cache {
+      FileSlotCache::Bytes(_, bytes) | FileSlotCache::Both(_, bytes) => {
+        bytes.clone()
+      }
+      FileSlotCache::Source(source) => {
+        let bytes = Bytes::from_string(source.text().to_owned());
+        self.cache = FileSlotCache::Both(source.clone(), bytes.clone());
+        bytes
+      }
+    }
+  }
+
+  fn source(&mut self) -> FileResult<Source> {
+    match &self.cache {
+      FileSlotCache::Source(source) | FileSlotCache::Both(source, _) => {
+        Ok(source.clone())
+      }
+      FileSlotCache::Bytes(id, bytes) => {
+        let bytes = bytes.clone();
+        let string = bytes
+          .as_str()
+          .map_err(|_| FileError::InvalidUtf8)?
+          .to_owned();
+        let source = Source::new(*id, string);
+        self.cache = FileSlotCache::Both(source.clone(), bytes);
+        Ok(source)
+      }
+    }
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,25 @@ use typst::syntax::{FileId, VirtualPath};
 use typst_html::HtmlDocument;
 
 fn main() {
-  let world = BlogWorld::new();
-  let world_with_main = Arc::new(world).with_main(FileId::new(
+  let world = Arc::new(BlogWorld::new());
+  let world_with_main = world.clone().with_main(FileId::new(
     None,
-    VirtualPath::new(Path::new("test.typ").canonicalize().unwrap()),
+    VirtualPath::new(Path::new("entry0.typ").canonicalize().unwrap()),
   ));
   let document = typst::compile::<HtmlDocument>(&world_with_main)
     .output
     .unwrap();
   // instead of stripping the html, we should template the html inside Typst.
   let buf = typst_html::html(&document).unwrap();
-  write(Path::new("out.html"), buf).unwrap();
+  write(Path::new("out0.html"), buf).unwrap();
+  let world_with_main = world.with_main(FileId::new(
+    None,
+    VirtualPath::new(Path::new("entry1.typ").canonicalize().unwrap()),
+  ));
+  let document = typst::compile::<HtmlDocument>(&world_with_main)
+    .output
+    .unwrap();
+  // instead of stripping the html, we should template the html inside Typst.
+  let buf = typst_html::html(&document).unwrap();
+  write(Path::new("out1.html"), buf).unwrap();
 }
