a1d9db476a5c3141ad3b7ea057975f2f5379056a
refparo <ref.paro@outlook.com>
2025-11-25 00:27:03 +0800

    Unify Source and Bytes; add cache invalidation and refresh

--- src/lib.rs
+++ src/lib.rs
@@ -11,13 +11,30 @@ use typst::utils::LazyHash;
 use typst::{Feature, Library, LibraryExt, World};
 use typst_kit::fonts::{FontSearcher, FontSlot, Fonts};
 
-pub struct BlogWorldWithMain {
+pub struct BlogMain {
   world: Arc<BlogWorld>,
   main: FileId,
+  dependencies: Mutex<Vec<FileId>>,
   now: chrono::DateTime<chrono::Utc>,
 }
 
-impl World for BlogWorldWithMain {
+impl BlogMain {
+  pub fn new(world: Arc<BlogWorld>, main: FileId) -> BlogMain {
+    BlogMain {
+      world,
+      main,
+      dependencies: Mutex::new(Vec::new()),
+      now: chrono::Utc::now(),
+    }
+  }
+
+  pub fn reset(&mut self) {
+    self.dependencies.lock().unwrap().clear();
+    self.now = chrono::Utc::now();
+  }
+}
+
+impl World for BlogMain {
   fn library(&self) -> &LazyHash<Library> {
     &self.world.library
   }
@@ -31,58 +48,17 @@ impl World for BlogWorldWithMain {
   }
 
   fn source(&self, id: FileId) -> FileResult<Source> {
-    let mut files = self.world.files.lock().unwrap();
-    if let Some(file) = files.get_mut(&id) {
-      file.source()
-    } else {
-      // I think it's better to take the risk of duplicating reads
-      // rather than making everyone else wait for this read to finish.
-      // Benchmarking also shows that even in serialized cases,
-      // releasing the lock and re-acquiring after the read finishes is faster
-      // than keeping the lock for the duration. (Why?)
-      drop(files);
-      let path = id.vpath().as_rooted_path();
-      let buf =
-        read_to_string(path).map_err(|err| FileError::from_io(err, path))?;
-      let source = Source::new(id, buf);
-      use std::collections::hash_map::Entry;
-      match self.world.files.lock().unwrap().entry(id) {
-        Entry::Occupied(entry) => entry.into_mut().source(),
-        Entry::Vacant(entry) => {
-          entry.insert(FileSlot::new(FileSlotCache::Source(source.clone())));
-          Ok(source)
-        }
-      }
-    }
+    self.dependencies.lock().unwrap().push(id);
+    self.world.access(id)
   }
 
   fn file(&self, id: FileId) -> FileResult<Bytes> {
-    let mut files = self.world.files.lock().unwrap();
-    if let Some(file) = files.get_mut(&id) {
-      Ok(file.bytes())
-    } else {
-      drop(files);
-      let path = id.vpath().as_rooted_path();
-      let buf = read(path).map_err(|err| FileError::from_io(err, path))?;
-      let bytes = Bytes::new(buf);
-      use std::collections::hash_map::Entry;
-      match self.world.files.lock().unwrap().entry(id) {
-        Entry::Occupied(entry) => Ok(entry.into_mut().bytes()),
-        Entry::Vacant(entry) => {
-          entry.insert(FileSlot::new(FileSlotCache::Bytes(id, bytes.clone())));
-          Ok(bytes)
-        }
-      }
-    }
+    self.dependencies.lock().unwrap().push(id);
+    self.world.access(id)
   }
 
   fn font(&self, index: usize) -> Option<Font> {
-    self
-      .world
-      .fonts
-      .fonts
-      .get(index)
-      .and_then(|slot| slot.get())
+    self.world.font(index)
   }
 
   fn today(&self, offset: Option<i64>) -> Option<Datetime> {
@@ -125,12 +101,48 @@ impl BlogWorld {
     }
   }
 
-  pub fn with_main(self: Arc<Self>, main: FileId) -> BlogWorldWithMain {
-    BlogWorldWithMain {
-      world: self,
-      main,
-      now: chrono::Utc::now(),
+  pub fn mark_sweep_reset(&self, ids: impl IntoIterator<Item = FileId>) {
+    let mut files = self.files.lock().unwrap();
+    for id in ids {
+      if let Some(file) = files.get_mut(&id) {
+        file.stale = true;
+      }
     }
+    files.retain(|_, file| {
+      if file.accessed || !file.stale {
+        file.accessed = false;
+        true
+      } else {
+        false
+      }
+    });
+  }
+
+  fn access<T: Cacheable>(&self, id: FileId) -> FileResult<T> {
+    let mut files = self.files.lock().unwrap();
+    if let Some(file) = files.get_mut(&id) {
+      file.access()
+    } else {
+      // I think it's better to take the risk of duplicating reads
+      // rather than making everyone else wait for this read to finish.
+      // Benchmarking also shows that even in serialized cases,
+      // releasing the lock and re-acquiring after the read finishes is faster
+      // than keeping the lock for the duration. (Why?)
+      drop(files);
+      let result = <T as Cacheable>::read(id)?;
+      use std::collections::hash_map::Entry;
+      match self.files.lock().unwrap().entry(id) {
+        Entry::Occupied(entry) => entry.into_mut().access(),
+        Entry::Vacant(entry) => {
+          entry.insert(FileSlot::new(result.clone().to_cache(id)));
+          Ok(result)
+        }
+      }
+    }
+  }
+
+  fn font(&self, index: usize) -> Option<Font> {
+    self.fonts.fonts.get(index).and_then(|slot| slot.get())
   }
 }
 
@@ -150,7 +162,8 @@ impl LazyHashFonts {
 }
 
 struct FileSlot {
-  // stale: bool,
+  accessed: bool,
+  stale: bool,
   cache: FileSlotCache,
 }
 
@@ -163,29 +176,64 @@ enum FileSlotCache {
 impl FileSlot {
   fn new(cache: FileSlotCache) -> FileSlot {
     FileSlot {
-      // stale: false,
+      accessed: true,
+      stale: false,
       cache,
     }
   }
 
-  // There is a redundant `clone()` in both of the following functions.
-  // However, we can't eliminate them without using `unsafe` or introducing
-  // an invalid state to `FileSlotCache`.
-  fn bytes(&mut self) -> Bytes {
-    match &self.cache {
-      FileSlotCache::Bytes(_, bytes) | FileSlotCache::Both(_, bytes) => {
-        bytes.clone()
-      }
-      FileSlotCache::Source(source) => {
-        let bytes = Bytes::from_string(source.text().to_owned());
-        self.cache = FileSlotCache::Both(source.clone(), bytes.clone());
-        bytes
-      }
+  fn access<T: Cacheable>(&mut self) -> FileResult<T> {
+    // This only tracks whether the slot is accessed during the previous
+    // iteration, rather than whether it is actually needed (for example,
+    // by a main that isn't refreshed during the last iteration.)
+    // So this appraoch is only viable if we wait for the compilation of every
+    // main to finish before starting the next iteration.
+    // This may be already good enough, though.
+    self.accessed = true;
+    if self.stale {
+      let id = self.id();
+      let result = <T as Cacheable>::read(id)?;
+      self.stale = false;
+      self.cache = result.clone().to_cache(id);
+      Ok(result)
+    } else {
+      Cacheable::from_cache(&mut self.cache)
     }
   }
 
-  fn source(&mut self) -> FileResult<Source> {
+  fn id(&self) -> FileId {
     match &self.cache {
+      FileSlotCache::Bytes(id, _) => *id,
+      FileSlotCache::Source(source) | FileSlotCache::Both(source, _) => {
+        source.id()
+      }
+    }
+  }
+}
+
+trait Cacheable: Clone + Sized {
+  fn read(id: FileId) -> FileResult<Self>;
+  fn to_cache(self, id: FileId) -> FileSlotCache;
+  fn from_cache(cache: &mut FileSlotCache) -> FileResult<Self>;
+}
+
+impl Cacheable for Source {
+  fn read(id: FileId) -> FileResult<Self> {
+    let path = id.vpath().as_rooted_path();
+    let buf =
+      read_to_string(path).map_err(|err| FileError::from_io(err, path))?;
+    Ok(Source::new(id, buf))
+  }
+
+  fn to_cache(self, _: FileId) -> FileSlotCache {
+    FileSlotCache::Source(self)
+  }
+
+  // There is a redundant `clone()` in both implementations.
+  // However, we can't eliminate them without using `unsafe` or introducing
+  // an invalid state to `FileSlotCache`.
+  fn from_cache(cache: &mut FileSlotCache) -> FileResult<Self> {
+    match cache {
       FileSlotCache::Source(source) | FileSlotCache::Both(source, _) => {
         Ok(source.clone())
       }
@@ -196,8 +244,33 @@ impl FileSlot {
           .map_err(|_| FileError::InvalidUtf8)?
           .to_owned();
         let source = Source::new(*id, string);
-        self.cache = FileSlotCache::Both(source.clone(), bytes);
+        *cache = FileSlotCache::Both(source.clone(), bytes);
         Ok(source)
+      }
+    }
+  }
+}
+
+impl Cacheable for Bytes {
+  fn read(id: FileId) -> FileResult<Self> {
+    let path = id.vpath().as_rooted_path();
+    let buf = read(path).map_err(|err| FileError::from_io(err, path))?;
+    Ok(Bytes::new(buf))
+  }
+
+  fn to_cache(self, id: FileId) -> FileSlotCache {
+    FileSlotCache::Bytes(id, self)
+  }
+
+  fn from_cache(cache: &mut FileSlotCache) -> FileResult<Self> {
+    match cache {
+      FileSlotCache::Bytes(_, bytes) | FileSlotCache::Both(_, bytes) => {
+        Ok(bytes.clone())
+      }
+      FileSlotCache::Source(source) => {
+        let bytes = Bytes::from_string(source.text().to_owned());
+        *cache = FileSlotCache::Both(source.clone(), bytes.clone());
+        Ok(bytes)
       }
     }
   }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,44 @@
 use std::fs::write;
+use std::io::BufRead;
 use std::path::Path;
 use std::sync::Arc;
 
-use blog::BlogWorld;
+use blog::{BlogMain, BlogWorld};
+use typst::World;
 use typst::syntax::{FileId, VirtualPath};
 use typst_html::HtmlDocument;
 
 fn main() {
   let world = Arc::new(BlogWorld::new());
-  let world_with_main = world.clone().with_main(FileId::new(
-    None,
-    VirtualPath::new(Path::new("entry0.typ").canonicalize().unwrap()),
-  ));
-  let document = typst::compile::<HtmlDocument>(&world_with_main)
-    .output
-    .unwrap();
-  // instead of stripping the html, we should template the html inside Typst.
-  let buf = typst_html::html(&document).unwrap();
-  write(Path::new("out0.html"), buf).unwrap();
-  let world_with_main = world.with_main(FileId::new(
-    None,
-    VirtualPath::new(Path::new("entry1.typ").canonicalize().unwrap()),
-  ));
-  let document = typst::compile::<HtmlDocument>(&world_with_main)
-    .output
-    .unwrap();
-  // instead of stripping the html, we should template the html inside Typst.
-  let buf = typst_html::html(&document).unwrap();
-  write(Path::new("out1.html"), buf).unwrap();
+  let main_ids = ["entry0.typ", "entry1.typ"].map(|filename| {
+    FileId::new(
+      None,
+      VirtualPath::new(Path::new(filename).canonicalize().unwrap()),
+    )
+  });
+  let mut mains = main_ids.map(|id| BlogMain::new(world.clone(), id));
+  let stdin = std::io::stdin();
+  loop {
+    for main in mains.iter() {
+      let document = typst::compile::<HtmlDocument>(&main).output.unwrap();
+      let buf = typst_html::html(&document).unwrap();
+      write(
+        main
+          .main()
+          .vpath()
+          .as_rooted_path()
+          .with_added_extension("html"),
+        buf,
+      )
+      .unwrap();
+    }
+    let mut buf = String::new();
+    if let Ok(0) | Err(_) = stdin.lock().read_line(&mut buf) {
+      break;
+    }
+    world.mark_sweep_reset(main_ids);
+    for main in mains.iter_mut() {
+      main.reset();
+    }
+  }
 }
